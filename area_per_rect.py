e23c7b219a108f94b4b6f926e00d31d1cf08c3c3
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-09-19 02:28:06 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Peter Sobowale
+# Date: Sept 18, 2022
+# This program calculates the area and perimeter of a rectangle.
+#   with dimensions 57cm x 19cm
+
+
+def main():
+    # This function calculates the area and perimeter 
+    print("If the rectangle has the dimensions:")
+    print("57cm x 19cm")
+    print()
+    print("The Area is {}cm^2". format(57*19))
+    print("The Perimeter is {}cm". format(2*(57+19)))
+
+
+if __name__ == "__main__":
+  main()
